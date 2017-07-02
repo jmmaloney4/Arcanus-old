56adbfbae576795a492aecea50af24de1cc0e16c
Jack Maloney <jmmaloney4@gmail.com>
2017-07-02 15:08:58 -0500

    Add Card Class and Names and Symbols

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,3 +1,93 @@
 import Foundation
 
 print("Hello, World!")
+
+struct CardConstants {
+    struct BloodfenRaptor {
+        static let name = "Bloodfen Raptor"
+        static let cost = 2
+        
+    }
+}
+
+class Card {
+    public enum Class {
+        case neutral
+        case druid
+        case hunter
+        case mage
+        case paladin
+        case priest
+        case rouge
+        case shaman
+        case warlock
+        case warrior
+        
+        public func getName() -> String {
+            switch self {
+            case .neutral: return "Neutral"
+            case .druid: return "Druid"
+            case .hunter: return "Hunter"
+            case .mage: return "Mage"
+            case .paladin: return "Paladin"
+            case .priest: return "Priest"
+            case .rouge: return "Rouge"
+            case .shaman: return "Shaman"
+            case .warlock: return "Warlock"
+            case .warrior: return "Warrior"
+            }
+        }
+        
+        public func getSymbol() -> Character {
+            switch self {
+            case .neutral: return "¤"
+            case .druid: return "🌿"
+            case .hunter: return "🏹"
+            case .mage: return "🔥"
+            case .paladin: return "🔨"
+            case .priest: return "✜"
+            case .rouge: return "⚔️"
+            case .shaman: return "🌋"
+            case .warlock: return "🖐️"
+            case .warrior: return "🗡"
+            }
+        }
+    }
+    
+    var name: String;
+    var cost: Int;
+    
+    init(withName name: String) {
+        self.name = name
+        self.cost = 0
+    }
+}
+
+print(Card.Class.warlock.getSymbol())
+print(Card.Class.priest.getSymbol())
+print(Card.Class.shaman.getSymbol())
+
+
+class Minion: Card {
+    
+}
+
+class BloodfenRaptor: Minion {
+    
+}
+
+class Spell: Card {
+    
+}
+
+class Weapon: Card {
+    
+}
+
+class HeroPower: Card {
+    
+}
+
+class Hero: Card {
+    
+}
