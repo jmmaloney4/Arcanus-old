5fb362ce1d04bd56da00584939239ea88121b5d2
Jack Maloney <jmmaloney4@gmail.com>
2017-07-06 00:58:51 -0500

    Update InstallSwift.sh

--- InstallSwift.sh
+++ InstallSwift.sh
@@ -4,11 +4,11 @@
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
 	DIR="$(pwd)"
 	cd ..
-	export SWIFT_VERSION=swift-3.1.1-RELEASE
+	# export SWIFT_VERSION=swift-3.1.1-RELEASE
 	wget https://swift.org/builds/swift-3.1.1-release/ubuntu1404/${SWIFT_VERSION}/${SWIFT_VERSION}-ubuntu14.04.tar.gz
 	tar xzf $SWIFT_VERSION-ubuntu14.04.tar.gz
 	ls ${PWD}/${SWIFT_VERSION}-ubuntu14.04/usr/bin
-	export PATH="${PWD}/${SWIFT_VERSION}-ubuntu14.04/usr/bin/:${PATH}"
+	# export PATH="${PWD}/${SWIFT_VERSION}-ubuntu14.04/usr/bin/:${PATH}"
 	cd "$DIR"
 	echo Path: $PATH
 else
